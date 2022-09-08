5f33b4082008e0c0b1a0c18c08ea195613a98d23
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2022-09-08 09:04:33 -0300

    Commit

--- Requisições_Assincronas/fetch1/script.js
+++ Requisições_Assincronas/fetch1/script.js
@@ -3,12 +3,28 @@ function loadPosts() {
         .then(function(resultado){
             return resultado.json()
             .then(function(json){
-                document.getElementById("posts").innerHTML = json.length + ' posts';
+                // Montamos o html com os dados.
+                montarBlog(json);
+                //document.getElementById("posts").innerHTML = json.length + ' posts';
+                console.log(json);
             })
         })
         .catch(function(erro){
             console.log('Deu Problema ' + erro)
         })
+}
+
+// método que irá montar o HTML com o resultado retornado na resposta da requisição.
+function montarBlog(lista) {
+    let html = '';
+    
+    for(let i in lista){
+        html += '<h3>' + lista[i].title + '</h3>';
+        html += '<p>' + lista[i].body + '</p><br/>';
+        html += '<hr/>';
+    }
+
+    document.getElementById('posts').innerHTML = html;
 }
 
 
